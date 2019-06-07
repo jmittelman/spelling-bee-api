692ebfea482198dac183db12cde5a4201dd4bb51
Jordan <jordanmittelman@gmail.com>
2019-06-07 12:23:41 -0400

    seeding with related fields excluded

--- seed.js
+++ seed.js
@@ -1,17 +1,32 @@
-const bees = require("./data/bees.json");
-const championsData = require("./data/champions.json");
-const sponsors = require("./data/sponsors");
-
-const championSeedData = championsData.map(champion => {});
-
 const Bee = require("./models/Bee");
 const Champion = require("./models/Champion");
 const Sponsor = require("./models/Sponsor");
 
-Bee.find({}).remove(() => Bee.create(bees).then(res => console.log(res)));
+const bees = require("./data/bees.json");
+const champions = require("./data/champions.json");
+const sponsors = require("./data/sponsors");
+
+const beesData = require("./data/bees.json");
+const championsData = require("./data/champions.json");
+
+const beeSeedData = beesData.map(bee => {
+  const championCut = JSON.parse(JSON.stringify(bee));
+  delete championCut.champion;
+  return championCut;
+});
+
+const championSeedData = championsData.map(champion => {
+  const sponsorCut = JSON.parse(JSON.stringify(champion));
+  delete sponsorCut.sponsor;
+  return sponsorCut;
+});
+
+Bee.find({}).remove(() =>
+  Bee.create(beeSeedData).then(res => console.log(res))
+);
 
 Champion.find({}).remove(() =>
-  Champion.create(champions).then(res => console.log(res))
+  Champion.create(championSeedData).then(res => console.log(res))
 );
 
 Sponsor.find({}).remove(() =>
